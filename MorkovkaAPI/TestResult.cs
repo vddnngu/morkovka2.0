550a6828999bf8fff7c2a84c94faf0ad005bed51
Kataurovaa <kataurovas@icloud.com>
2020-02-19 23:04:01 +0300

    TestResult

---
+++ MorkovkaAPI/TestResult.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorkovkaAPI
+{
+    public class Date
+    {
+        int day;
+        int month;
+        int year;
+        public Date(string d)
+        {
+            string[] strs = d.Split('.');
+            day = Convert.ToInt32(strs[0]);
+            month = Convert.ToInt32(strs[1]);
+            year = Convert.ToInt32(strs[2]);
+        }
+    }
+    public class Time
+    {
+        int hour;
+        int minute;
+        int seconds;
+        public Time(string t)
+        {
+            string[] strs = t.Split(':');
+            hour = Convert.ToInt32(strs[0]);
+            minute = Convert.ToInt32(strs[1]);
+            seconds = Convert.ToInt32(strs[2]);
+        }
+        public static int operator-(Time op1, Time op2)
+        {
+            int h = op1.hour - op2.hour;
+            int m= op1.minute - op2.minute;
+            int s= op1.seconds - op2.seconds;
+            return h * 3600 + m * 60 + s;
+        }
+
+    }
+    public class TestResult
+    {
+        string name;
+        Date date;
+        Time start;
+        Time finish;
+        TestProcessing test;
+        List<int> answers;
+        
+        public void setName(string _name)
+        {
+            name = _name;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public void setDate(Date _date)
+        {
+            date = _date;
+        }
+        public Date getDate()
+        {
+            return date;
+        }
+        public void setTimeStart(Time _start)
+        {
+            start = _start;
+        }
+        public Time getTimeStart()
+        {
+            return start;
+        }
+        public void setTimeFinish(Time _finish)
+        {
+            finish = _finish;
+        }
+        public Time getTimeFinish()
+        {
+            return finish;
+        }
+        public void setTestProcessing(TestProcessing _test)
+        {
+            test = _test;
+        }
+        public TestProcessing getTestProcessing()
+        {
+            return test;
+        }
+        public void setListAnswers(List<int> _answers)
+        {
+            answers = _answers;
+        }
+        public List<int> getListAnswers()
+        {
+            return answers;
+        }
+        public TestResult()
+        {
+            answers = new List<int>();
+        }
+    }
+}
---
+++ MorkovkaAPI/TestResultReader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorkovkaAPI
+{
+    public class ResEntity
+    {
+        public string name;
+        public string date;
+        public string start;
+        public string finish;
+        public string result;
+        public string testPath;
+        
+    }
+    public class TestResultParser
+    {
+        string path;
+        FileStream file;
+        StreamReader fin;
+        string testPath;
+        List<ResEntity> resEntities;
+        public TestResultParser(string _path)
+        {
+            path = _path;
+            file = new FileStream(path, FileMode.Open);
+            fin = new StreamReader(file);
+
+        }
+        void ParseHeader()
+        {
+            string tmp;
+            while ((tmp = fin.ReadLine()) != "END HEADER")
+            {
+                if (tmp == "") continue;
+                string[] strs = tmp.Split('|');
+                if (strs[0] == "TestPath")
+                {
+                    testPath = strs[1];
+                    continue;
+                }  
+            }
+        }
+        void Parse()
+        {
+            string tmp;
+            while ((tmp = fin.ReadLine()) != "END")
+            {
+                string[] strs = tmp.Split('|');
+                ResEntity entity = new ResEntity();
+                entity.name = strs[0];
+                entity.date = strs[1];
+                entity.start = strs[2];
+                entity.finish = strs[3];
+                entity.result = strs[4];
+                entity.testPath = testPath;
+                resEntities.Add(entity);
+            }
+        }
+        
+    }
+    public class ResultCreator
+    {
+        List<TestResult> testResults;
+        TestResult entityHandler(ResEntity _resEntity)
+        {
+            TestResult result = new TestResult();
+            result.setName(_resEntity.name);
+            result.setDate(new Date(_resEntity.date));
+            result.setTimeStart(new Time(_resEntity.start));
+            result.setTimeFinish(new Time(_resEntity.finish));
+            string[] strs = _resEntity.result.Split(' ');
+            for (int i = 0; i<strs.Length; i++)
+            {
+                result.getListAnswers().Add(Convert.ToInt32(strs[i]));
+            }
+            return result;
+        }
+    }
+
+}
